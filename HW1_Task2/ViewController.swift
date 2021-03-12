48ee34f15bd7c3cec67a4975168234ea9de1d64a
Gr1ngoire <gringoirelamusicart@gmail.com>
2021-03-12 19:15:32 +0200

    Initial Commit

---
+++ HW1_Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1_Task2
+//
+//  Created by admin on 12.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
